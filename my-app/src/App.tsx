03a00ce2e010ab95f5197eaadf007fc8f2779d3d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-06 21:37:27 +0100

    dod. komp. GameBoard do renderowania (na razie sam szkielet/wyglad)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Board from './board/Board';
+import GameBoard from './conteners/GameBoard';
+
 import './App.css';
 
 const App: React.FC = () => {
+
+    const [gameBoard, setGameBoard] = useState(new Board());
+
     return (
         <div className="App">
             <p>React App</p>
+            <GameBoard gameBoard={gameBoard} />
         </div>
     );
 }
