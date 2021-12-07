eda2d793c7a5b221905be8ea2899faf40c51fbf9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-07 20:32:34 +0100

    dod. met. _swapNumsOnBoard

--- my-app/src/board/Board.ts
+++ my-app/src/board/Board.ts
@@ -37,6 +37,30 @@ class Board {
     private _getLocOfEmpty(): number[] {
         return this._getLocOfNum(16); // 16 is empty
     }
+
+    private _isMoveLegal(numToMove: number): boolean {
+        let [mvRow, mvCol] = this._getLocOfNum(numToMove);
+        let [emptyRow, emptyCol] = this._getLocOfEmpty();
+        if (mvRow === emptyRow) {
+            if ((mvCol - 1) === emptyCol || (mvCol + 1) === emptyCol) {
+                return true;
+            }
+        } else if (mvCol === emptyCol) {
+            if ((mvRow - 1) === emptyRow || (mvRow + 1) === emptyRow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private _swapNumsOnBoard(num1: number, num2: number): void {
+        let [n1row, n1col] = this._getLocOfNum(num1);
+        let [n2row, n2col] = this._getLocOfNum(num2);
+        let v1: number = this._board[n1row][n1col].getVal();
+        let v2: number = this._board[n2row][n2col].getVal();
+        this._board[n1row][n1col].setVal(v2);
+        this._board[n2row][n2col].setVal(v1);
+    }
 }
 
 export default Board;
