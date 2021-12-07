620d2d7e4a28874f0867c9adbb011f3a4a32c843
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-07 20:01:23 +0100

    dod. metody _getLocOfNum, getLocOfEmpty

--- my-app/src/board/Board.ts
+++ my-app/src/board/Board.ts
@@ -20,6 +20,23 @@ class Board {
     public getBoard(): Field[][] {
         return this._board;
     }
+
+    private _getLocOfNum(searchedNum: number): number[] {
+        let result: number[] = [];
+        for (let r = 0; r < this.getBoard().length; r++) {
+            for (let c = 0; c < this.getBoard()[r].length; c++) {
+                if (this.getBoard()[r][c].getVal() === searchedNum) {
+                    result = [r, c];
+                    break;
+                }
+            }
+        }
+        return result
+    }
+
+    private _getLocOfEmpty(): number[] {
+        return this._getLocOfNum(16); // 16 is empty
+    }
 }
 
 export default Board;
