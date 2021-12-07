b83f9b3b9e1c3bf0d84c1c90d2a44d5cc0e52d75
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-12-07 20:30:17 +0100

    kosmetyka, dod. met. setVal

--- my-app/src/board/Field.ts
+++ my-app/src/board/Field.ts
@@ -18,6 +18,10 @@ class Field {
         return this._val;
     }
 
+    public setVal(newVal: number): void {
+        this._val = newVal;
+    }
+
     public toString(): string {
         if (this.getVal() === 16) { // 16 is empty field
             return "";
