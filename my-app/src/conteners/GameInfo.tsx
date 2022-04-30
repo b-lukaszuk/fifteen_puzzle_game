b70bd31036d7fd7b3aad01b56e1c4cebdc08daf7
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-04-30 08:34:49 +0200

    kosmetyka

--- my-app/src/conteners/GameInfo.tsx
+++ my-app/src/conteners/GameInfo.tsx
@@ -22,13 +22,14 @@ const GameInfo: React.FC<Props> = (props) => {
                 </a>
             </span>
             <br />
-            <span><b>Game status:</b> {gameStatusIn}</span>
-            <br />
             <b>Instructions: </b>
             <br />
             <span>Click on a field near the empty field to shift it to that spot</span>
             <br />
             <span>Order all numbers from 1 to 15 (left to right, top to bottom)</span>
+            <br /> <br />
+            <span><b>Game status:</b> {gameStatusIn}</span>
+            <br />
         </div >
     );
 }
